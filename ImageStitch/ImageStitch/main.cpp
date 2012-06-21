d8f34d1b909feed3c13164d6649c83fe047422f4
scarlettDiMa <dma4@binghamton.edu>
2012-06-21 14:44:41 -0400

    this branch is for research material

--- ImageStitch/ImageStitch/main.cpp
+++ ImageStitch/ImageStitch/main.cpp
@@ -1,8 +1,8 @@
-// Jason: I haven't included OpenCV's header files nor library files.
+﻿// Jason: I haven't included OpenCV's header files nor library files.
 //        You may need to edit your property pages.
 
 int main()
 {
-	std::cout<<“3 hours a day”<<endl;
+	std::cout<<"3 hours a day"<<endl;
 	return 0;
 }
